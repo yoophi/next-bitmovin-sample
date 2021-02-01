e9801df6fec2da1cec0f570bd8261b5208be77b1
Pyunghyuk Yoo <yoophi@gmail.com>
2021-02-01 18:33:48 +0900

    feat: add bootstrap, react-bootstrap

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        hello, world
+        <h1>hello, world</h1>
       </main>
 
     </div>
